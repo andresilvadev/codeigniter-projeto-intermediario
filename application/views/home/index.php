df6c4fb72c943206c8b5bb1543c0eb27240eeb47
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-20 14:44:13 -0300

    Trabalhando com URL absoluto, base_url com helper url do codeigniter

---
+++ application/views/home/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php echo base_url('public/css/bootstrap.min.css'); ?>">
+        <title>Area 51</title>
+    </head>
+    <body>
+        <header>
+            <h1>Area de testes</h1> 
+        </header>
+        <hr>
+    </body>
+</html>
