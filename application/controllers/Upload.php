9eca77dbfd4d2406a21fbb6e5d73d77b0065a573
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-21 12:02:08 -0300

    Criando sistema de upload

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -13,7 +13,26 @@ class Upload extends CI_Controller
 
     public function subir()
     {
-        
+        $nome = $this->input->post('nome');
+        $arquivo = $_FILES['arquivo'];
+
+        $nome_limpo = trim($nome);
+
+        $config = array(
+            "upload_path" => "public/upload",
+            "allowed_types" => "pdf",
+            "file_name" => rand(1,987695) . '_' . $nome_limpo . '.pdf',
+            "max_size" => "300000"
+        );
+
+        $this->load->library('upload');
+        $this->upload->initialize($config); // initialize prepara o processo de upload
+
+        if ($this->upload->do_upload('arquivo') === true) {  // do_upload recebe a variavel arquivo entre plicas, retornando true ou false
+            echo "Arquivo upado";
+        } else {
+            echo $this->upload->display_errors();
+        }
     }
 
 
